f53863608564bba29eeb1ab1c4c6dd565bcd23c7
aumabansag <aumabansag@up.edu.ph>
2021-05-20 16:08:54 +0900

    Updated SaveSudoku + moved subgrid-new
    SaveSudoku converts inputs to given if there are no existing givens yet (when creating a new puzzle)

--- src/SaveSudoku.java
+++ src/SaveSudoku.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 public class SaveSudoku{
   private FileWriter writer;
   private PrintWriter print;
+  public SaveSudoku(){
+  }
   protected int save(String fileName, int[][][] sudokuArray){
     try{
       File file = new File("save/"+fileName+".sav");
@@ -16,11 +18,23 @@ public class SaveSudoku{
       writer=new FileWriter("save/"+fileName+".sav");
       print= new PrintWriter(writer);
       print.print(sudokuArray.length+"\n");
+
+      boolean freshBoard = isFresh(sudokuArray);
       for(int x=0; x<sudokuArray.length; x++){
         String num="", type="";
         for(int y=0; y<sudokuArray.length; y++){
           num=num+sudokuArray[x][y][0]+" ";
-          type=type+sudokuArray[x][y][1]+" ";
+          if(freshBoard){
+            if(sudokuArray[x][y][0] > 0){
+              type = type + "0 ";
+            }
+            else{
+              type = type + "1 ";
+            }
+          }
+          else{
+            type=type+sudokuArray[x][y][1]+" ";
+          }
         }
         print.print(num+"\n");
         print.print(type+"\n");
@@ -34,5 +48,15 @@ public class SaveSudoku{
   
   protected void delete(String filename){
     new File("save/"+filename+".sav").delete();
+  }
+  private boolean isFresh(int[][][] sudokuArray){
+    for(int i = 0; i < sudokuArray.length; i++){
+      for(int j = 0; j < sudokuArray.length; j++){
+        if(sudokuArray[i][j][1] == 0){
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
